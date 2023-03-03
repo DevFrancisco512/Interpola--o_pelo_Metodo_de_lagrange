4ba4a80efee69c07156746d88d6c5c312630b904
Francisco Inácio <netofrancisco512@gmail.com>
2023-03-03 19:25:32 -0300

    Reformulando a interface

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from tkinter import *
+import tkinter as tki
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 
 # Por meio da interpolação de Lagrange a função retorna  yp do xp correspondente
-def interpolacaoLagrange(x, vetx, vety) -> float:
+def interpolLagrange(x, vetx, vety) -> float:
     yp = 0
     indexy = 0
     for xi in vetx:
@@ -17,55 +18,40 @@ def interpolacaoLagrange(x, vetx, vety) -> float:
         
     return yp
 
-def lerEntradas():
-    entradas = None
-    cond = True
-    while cond:
-        try:
-            print('Interpolação de Lagrange\n','='*30,'\n')
-            entradas = [input(f'x{i}, y{i}:').split(',') for i in range(3)]
-            entradas.append(input('Ponto px: '))
-            cond = False
-        except Exception as e:
-            print(e)
-            continue
-    return entradas
+class tela():
+    def __init__(self) -> None:
+        root = Tk()
+        root.title('Método de lagrange')
+        wsize = root.winfo_screenwidth(), root.winfo_screenheight()
+        center = wsize[0]
+        root.geometry('820x480')
+
+        backgrafic = tki.Frame(root, width=640, height=480)
+        backgrafic.grid(column=1, row=1,rowspan=3)
+        content = tki.Frame(root, width=180, height=480, bg='grey')
+        content.grid(column=0, row=0, rowspan=3)
+
+        strvet1 = StringVar()
+        entrytvet1 = Entry(content,font=('Arial 14'))
+        x, y = [45, 10]
+        entrytvet1.place(x=45, y=10, width=130)
+
+    def genGrafico(self, vetx, vety, n, xp, yp):
+        minX = min(vetx)
+        maxX = max(vetx)
+        w = np.arange(minX-minX%10, maxX+maxX%10, 0.01)
+        yw = []
+        for pos in w:
+            yw.append(interpolLagrange(pos, vetx, vety))
+        
+        ax = plt.axes()
+        ax.plot(w, yw, 'b-')
+        ax.plot(vetx, vety, 'ro')
+        ax.plot(xp, yp, 'go')
+        ax.set_ylabel('eixo y')
+        ax.set_xlabel('eixo x')
+        plt.savefig('images/grafico.png')
+
 
 if __name__ == '__main__':
-    entrada = lerEntradas()
-    vetx = list()
-    vety = list()
-    for i in range(3):
-        vetx.append(float(entrada[i][0]))
-        vety.append(float(entrada[i][1]))
-    tm = 2
-    xp = float(entrada[3])
-    yp = interpolacaoLagrange(xp, vetx, vety)
-
-    #Janela tk
-    janela = Tk()
-    janela.title('Método de Lagrange')
-    janela.geometry('800x430')
-    janela['bg'] = 'grey'
-    figura = plt.Figure(figsize=(8, 4), dpi=100)
-    ax = figura.add_subplot(111)
-
-    canva = FigureCanvasTkAgg(figura, janela)
-    canva.get_tk_widget().grid(row=0, column=0)
-
-
-    # Fixar estado aleatório para produção
-    np.random.seed(19680801)
-
-    # Plotando gráfico
-    w = np.arange(-8, 8, 0.1)
-    yw = []
-    for pos in w:
-        yw.append(interpolacaoLagrange(pos, vetx, vety))
-    ax.plot(w, yw, 'b-')
-    ax.plot(vetx, vety, 'ro')
-    ax.plot(xp, yp, 'go')
-    ax.set_ylabel('eixo y')
-    ax.set_xlabel('eixo x')
-
-    janela.mainloop()
+    tela()
--- teste.py
+++ teste.py
@@ -1,23 +1,6 @@
-'''import matplotlib.pyplot as plt
-import numpy as np
+from tkinter import *
+from tkinter import ttk
 
-plt.style.use('_mpl-gallery')
-
-# make data
-x = np.linspace(0, 10, 100)
-y = 4 + 2 * np.sin(2 * x)
-
-# plot
-fig, ax = plt.subplots()
-
-ax.plot(x, y, linewidth=2.0)
-
-ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
-       ylim=(0, 8), yticks=np.arange(1, 8))
-
-plt.show()'''
-
-
-vet = [1, 2, 4]
-
-print(2*vet)
+root = Tk()
+root.geometry('100x100')
+root.mainloop()
